62455cf16e203b39beda1c253918c7159b388cee
Mark Piper <mark.piper@colorado.edu>
2015-11-04 15:18:06 -0700

    Update run_swash script to use local storage on compute node

--- run_swash.sh
+++ run_swash.sh
@@ -1,22 +1,32 @@
 #! /usr/bin/env bash
+# A `qsub` submission script that calls SWASH with `mpiexec`.
 #
-# Calls SWASH with `mpiexec`.
-#
-#PBS -l nodes=2:ppn=8
-#PBS -l walltime=18:00:00
+#PBS -l nodes=1:ppn=8
+#PBS -l walltime=12:00:00
 #PBS -m abe
 #PBS -M mark.piper@colorado.edu
 
-NCPU=`wc -l < $PBS_NODEFILE`
-NNODES=`uniq $PBS_NODEFILE | wc -l`
-MPIEXEC=/opt/openmpi/bin/mpiexec
-CMD="$MPIEXEC -np $NCPU --verbose"
+ncpu=`wc -l < $PBS_NODEFILE`
+nnodes=`uniq $PBS_NODEFILE | wc -l`
+mpiexec=/opt/openmpi/bin/mpiexec
+swash=/home/csdms/models/swash/bin/swash-mpi
+runcmd="$mpiexec -np $ncpu $swash INPUT"
 
-echo "--> Running on nodes " `uniq $PBS_NODEFILE`
-echo "--> Number of available cpus " $NCPU
-echo "--> Number of available nodes " $NNODES
-echo "--> Launch command is " $CMD
+# Create a working directory on the compute node. Copy the contents of
+# the original PBS working directory to it.
+working=/state/partition1/$PBS_JOBNAME-$PBS_JOBID
+if [ ! -d $working ]; then
+    mkdir $working
+fi
+trap "rm -rf $working" EXIT
+cd $working && cp $PBS_O_WORKDIR/* .
 
-cd $PBS_O_WORKDIR
-$CMD ../swash_mpi.exe INPUT
+echo "--> Running on nodes: " `uniq $PBS_NODEFILE`
+echo "--> Number of available cpus: " $ncpu
+echo "--> Number of available nodes: " $nnodes
+echo "--> Run command: " $runcmd
+echo "--> Working directory: " $working
+$runcmd
 
+# Copy the completed run back to the original PBS working directory.
+cp -R $working/* $PBS_O_WORKDIR
